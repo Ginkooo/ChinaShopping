03d1d36c5f65ac892b19a61101d39f721f322da5
Piotr Czajka <piotr_czajka@outlook.com>
2017-02-25 10:33:22 +0100

    pypath print at launch

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+    print(sys.path)
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CHINA_ROOT.settings")
     try:
         from django.core.management import execute_from_command_line
